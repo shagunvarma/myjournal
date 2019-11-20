3ee12ac06205b02311df6dac08939e9d1a703176
Ethan <ejh4@illinois.edu>
2019-11-19 22:12:53 -0600

    Initial commit

---
+++ app/src/main/java/com/example/mentalhealth/NewJournalEntry.java
@@ -0,0 +1,14 @@
+package com.example.mentalhealth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewJournalEntry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_journal_entry);
+    }
+}
